6c1e20b80b44eca6226f67f15a1c560c01eca5c6
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-23 15:06:01 -0500

    commit tree f5d8c25ddd6603160f41ef04f79694e5a8740b3e

--- src/index.js
+++ src/index.js
@@ -1,1 +1,79 @@
-console.log('%c HI', 'color: firebrick')
+let breeds = []
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  loadImages();
+  allDogBreeds();
+})
+
+function loadImages(){
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+  fetch(imgUrl)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json){
+      json.message.forEach(image => addImage(image))
+    })
+}
+
+function addImage(dogPic){
+  let container = document.getElementById('dog-image-container');
+  let newImageEl = document.createElement('img');
+  newImageEl.src = dogPic;
+  container.appendChild(newImageEl);
+}
+
+function allDogBreeds(){
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
+  fetch(breedUrl)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json){
+      breeds = Object.keys(json.message);
+      updateBreedList(breeds);
+      addBreedSelectListener();
+    })
+}
+
+function updateBreedList(breeds){
+  let ul = document.getElementById('dog-breeds');
+  removeChildren(ul);
+  breeds.forEach(breed => addBreed(breed));
+}
+
+function removeChildren(element){
+  let child = element.lastElementChild;
+  while(child){
+    element.removeChild(child);
+    child = element.lastElementChild;
+  }
+}
+
+function selectBreedsStartingWith(letter){
+  updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+}
+
+function addBreedSelectListener(){
+  let breedDropdown = document.getElementById('breed-dropdown');
+  breedDropdown.addEventListener('change', function(e){
+    selectBreedsStartingWith(e.target.value);
+  })
+}
+
+function addBreed(breed) {
+  let ul = document.querySelector('#dog-breeds');
+  let li = document.createElement('li');
+  li.innerText = breed;
+  li.style.cursor = 'pointer';
+  ul.appendChild(li);
+  li.addEventListener('click', updateColor);
+}
+
+function updateColor(event) {
+  event.target.style.color = 'magenta';
+}
+
+
+
